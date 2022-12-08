21afeaa1d4a290478d8ccf8f95593d872aa24d7c
FutteKudasai <oliver.tsaoyh@gmail.com>
2022-12-08 19:39:00 +0800

    first add to github

---
+++ 2022-11-24/2022-11-24/2022-11-24/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace _2022_11_24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(96, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "局定勝負？";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label2.Location = new System.Drawing.Point(432, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "還有局";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label3.Location = new System.Drawing.Point(432, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "贏";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label4.Location = new System.Drawing.Point(495, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 27);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "平";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label5.Location = new System.Drawing.Point(560, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 27);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "輸";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label6.Location = new System.Drawing.Point(446, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 27);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label7.Location = new System.Drawing.Point(509, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 27);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label8.Location = new System.Drawing.Point(574, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 27);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label9.Location = new System.Drawing.Point(239, 201);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(174, 27);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "請按鈕出拳！";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.Location = new System.Drawing.Point(101, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 90);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "剪刀";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.Location = new System.Drawing.Point(262, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 90);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "石頭";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.Location = new System.Drawing.Point(426, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 90);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "布";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "剪刀.gif");
+            this.imageList1.Images.SetKeyName(1, "石頭.gif");
+            this.imageList1.Images.SetKeyName(2, "布.gif");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(117, 179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(437, 179);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 428);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ 2022-11-24/2022-11-24/2022-11-24/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2022_11_24
+{
+    public partial class Form1 : Form
+    {
+        int round, number, win=0, draw=0, lose=0;
+        string choose;
+        Random random_number = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "1";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (0 == Convert.ToInt32(textBox1.Text))
+                {
+                    textBox1.Text = "1";
+                }
+                label2.Text = "還有" + textBox1.Text + "局";
+                round = Convert.ToInt32(textBox1.Text);
+                label9.Text = "請按鈕出拳！";
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                win = draw = lose = 0;
+                label6.Text = "0";
+                label7.Text = "0";
+                label8.Text = "0";
+            }
+            catch { }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button choose = (Button)sender;//偵測按鈕
+            textBox1.ReadOnly = true;
+            round--;
+            label2.Text = "還有" + round + "局";
+            number = random_number.Next(3);//生成亂數
+            pictureBox2.Image = imageList1.Images[Convert.ToInt32(number)];
+            if (choose == button1)
+            {
+                pictureBox1.Image = imageList1.Images[0];
+                if(0 == number)
+                {
+                    draw++;
+                    label7.Text = Convert.ToString(draw);
+                }
+                if (1 == number)
+                {
+                    lose++;
+                    label8.Text = Convert.ToString(lose);
+                }
+                if (2 == number)
+                {
+                    win++;
+                    label6.Text = Convert.ToString(win);
+                }
+            }
+            if (choose == button2)
+            {
+                pictureBox1.Image = imageList1.Images[1];
+                if (0 == number)
+                {
+                    win++;
+                    label6.Text = Convert.ToString(win);
+                    
+                }
+                if (1 == number)
+                {
+                    draw++;
+                    label7.Text = Convert.ToString(draw);
+                    
+                }
+                if (2 == number)
+                {
+                    lose++;
+                    label8.Text = Convert.ToString(lose);
+                }
+            }
+            if (choose == button3)
+            {
+                pictureBox1.Image = imageList1.Images[2];
+                if (0 == number)
+                {
+                    lose++;
+                    label8.Text = Convert.ToString(lose);
+                    
+                }
+                if (1 == number)
+                {
+                    win++;
+                    label6.Text = Convert.ToString(win);
+                }
+                if (2 == number)
+                {
+                    draw++;
+                    label7.Text = Convert.ToString(draw);
+                }
+            }
+            
+            if (0 >= round)
+            {
+                textBox1.ReadOnly = false;
+                if(win > lose)
+                {
+                    label9.Text = "你贏了";
+                }
+                if (win < lose)
+                {
+                    label9.Text = "你輸了";
+                }
+                if (win == lose)
+                {
+                    label9.Text = "平手";
+                }
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+        }
+    }
+}
